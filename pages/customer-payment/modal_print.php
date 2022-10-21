ade699ccd1c88f97dcb2cbe0380363ec5b9b80d4
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-10-21 16:11:01 +0800

    print

--- pages/customer-payment/modal_print.php
+++ pages/customer-payment/modal_print.php
@@ -37,7 +37,7 @@
              <div class="modal-footer">
                  <button type="button" onclick="print_report('print_canvas')" class="btn btn-primary ml-1 btn-sm">
                      <i class="bx bx-check d-block d-sm-none"></i>
-                     <span class="d-none d-sm-block"><span class='ti ti-printer'></span> Printer</span>
+                     <span class="d-none d-sm-block"><span class='ti ti-printer'></span> Print</span>
                  </button>
              </div>
          </div>
