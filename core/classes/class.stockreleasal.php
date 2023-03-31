6106a178a5b9c90878e7496ea1c76462bc85a3e8
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2023-03-31 14:52:08 +0800

    Stock Releasal Report
    - Add Per Item

--- core/classes/class.stockreleasal.php
+++ core/classes/class.stockreleasal.php
@@ -3,6 +3,7 @@
 class StockReleasal extends Connection
 {
     public $table = "tbl_sales";
+
     public function view()
     {
         $customer_id = $this->inputs['customer_id'];
@@ -33,18 +34,34 @@ class StockReleasal extends Connection
 
             if ($result->num_rows > 0) {
                 $counter += 1;
-                $data .= '<table class="table" style="margin-bottom: 50px;"><thead><tr><th colspan="7" style="background: #607d8b;color: #fff;">Customer: '.$Customer->name($cRow["customer_id"]).'</th></tr><tr><th>DATE</th><th>REFERENCE #</th><th>PAYMENT STATUS</th><th>BALANCE</th><th>PRODUCT</th><th style="text-align:right;">TOTAL QTY</th><th style="text-align:right;">REMAINING QTY</th></tr></thead><tbody>';
+                $data .= '<table class="table" style="margin-bottom: 50px;"><thead><tr><th colspan="7" style="background: #607d8b;color: #fff;">Customer: '.$Customer->name($cRow["customer_id"]).'</th></tr><tr><th>DATE</th><th>REFERENCE #</th><th>PRODUCT</th><th style="text-align:right;">TOTAL QTY</th><th style="text-align:right;">REMAINING QTY</th></tr></thead><tbody>';
                 $Sales = new Sales;
                 while ($row = $result->fetch_assoc()) {
                     $remaining_qty = $StockWithdrawal->remaining_qty($row['sales_detail_id']);
-                    $paid_status = $row['paid_status'] == 1 ? "<label class='badge badge-success'>Paid</label>" : "<label class='badge badge-warning'>Unpaid</label>";
-                    $bal = $row['paid_status'] != 1 ? "₱ " . number_format($Sales->dr_balance($row['sales_id']), 2) : "0.00" ;
+                    // $paid_status = $row['paid_status'] == 1 ? "<label class='badge badge-success'>Paid</label>" : "<label class='badge badge-warning'>Unpaid</label>";
+                    // $bal = $row['paid_status'] != 1 ? "₱ " . number_format($Sales->dr_balance($row['sales_id']), 2) : "0.00" ;
+
+                    // if($row['paid_status'] == 1){
+                    //     $paid_status = "<label class='badge badge-success'>Paid</label>";
+                    //     $bal = "0.00";
+                    // }else{
+                    //     $r_bal = $Sales->dr_balance($row['sales_id']);
+                    //     if($r_bal > 0){
+                    //         $bal = "₱ " . number_format($r_bal,2);
+                    //         $paid_status = "<label class='badge badge-warning'>Unpaid</label>";
+                    //     }else{
+                    //         $bal = "₱ " . number_format($r_bal,2);
+                    //         $paid_status = "<label class='badge badge-success'>Paid</label>";
+                    //     }
+                        
+                    // }
+
                     if ($remaining_qty > 0) {
                         $data .= "<tr>";
                         $data .= "<td>" . date('M d,Y', strtotime($row['sales_date'])) . "</td>";
                         $data .= "<td>" . $row['reference_number'] . "</td>";
-                        $data .= "<td>" . $paid_status . "</td>";
-                        $data .= "<td>" . $bal ."</td>";
+                        // $data .= "<td>" . $paid_status . "</td>";
+                        // $data .= "<td>" . $bal ."</td>";
                         $data .= "<td>" . $Products->name($row['product_id']) . "</td>";
                         $data .= "<td style='text-align: right;'>" . number_format($row['total_qty'], 2) . "</td>";
                         $data .= "<td style='text-align: right;'>" . number_format($remaining_qty, 2) . "</td>";
@@ -56,5 +73,37 @@ class StockReleasal extends Connection
         }
         
         echo $counter > 0 ? $data : "<hr><center style='color: #757575;'><h3>No details found.</h3></center><hr>";
+    }
+
+    public function per_item()
+    {
+        $product_id = $this->inputs['product_id'];
+
+        if ($product_id == "-1") {
+            $prod_param = "";
+        } else {
+            $prod_param = "AND d.product_id='$product_id'";
+        }
+        $StockWithdrawal = new StockWithdrawal;
+        $counter = 0;
+        $result = $this->select("tbl_products","*",$prod_param);
+        while ($row = $result->fetch_assoc()) {
+            //$result = $this->select("tbl_sales as sh, tbl_sales_details as sd, tbl_stock_withdrawal as wh, tbl_stock_withdrawal_details as wd", "sum(sd.quantity) as dr_qty", "sd.product_id='$prow[product_id]' AND sh.sales_id=sd.sales_id AND sh.status='F' AND wh.sales_id=sh.sales_id AND wh.withdrawal_id=wd.withdrawal_id AND wd.product_id='$prow[product_id]' AND wh.status='F'");
+            $fetchSAles = $this->select("tbl_sales as h, tbl_sales_details as d","*","d.product_id='$row[product_id]' AND h.sales_id=d.sales_id AND h.status='F' AND h.withdrawal_status='1'");
+            $for_withdrawal = 0;
+            while($sRow = $fetchSAles->fetch_array()){
+                $remaining_qty = $StockWithdrawal->remaining_qty($sRow['sales_detail_id']);
+                $for_withdrawal += $remaining_qty;
+            }
+
+            $row['item'] = $row['product_name'];
+            $row['on_hand'] = number_format($for_withdrawal,2);
+            $row['for_withdrawal'] = number_format($for_withdrawal,2);
+            $row['available'] = number_format($for_withdrawal,2);
+                
+            $rows[] = $row;
+        }
+
+        return $rows;
     }
 }
--- pages/stock-releasal/index.php
+++ pages/stock-releasal/index.php
@@ -1,72 +1,161 @@
 <div class="content-wrapper">
     <div class="row">
         <div class="col-md-12 grid-margin">
-            <div class="row">
-                <div class="col-12 col-xl-12 mb-4 mb-xl-0">
-                    <h3 class="font-weight-bold">Stock Releasal Report</h3>
-                    <h6 class="font-weight-normal mb-0">Generate stock releasal here</h6>
+            <div class="col-md-12 grid-margin">
+                <div class="row">
+                    <div class="col-12 col-xl-12 mb-4 mb-xl-0">
+                        <h3 class="font-weight-bold">Stock Releasal Report</h3>
+                        <h6 class="font-weight-normal mb-0">Generate stock releasal here</h6>
+                    </div>
                 </div>
-            </div>
+                <br>
+                <br>
+                <div class="card shadow mb-4">
+                    <ul id="tabs" class="nav nav-tabs">
+                        <li class="nav-item"><a href="" data-target="#per_transaction" data-toggle="tab" class="nav-link small text-uppercase active">Per Transaction</a></li>
+                        <li class="nav-item"><a href="" data-target="#per_item" data-toggle="tab" class="nav-link small text-uppercase">Per Item</a></li>
+                    </ul>
+                    <br>
+                    <div id="tabsContent" class="tab-content">
+                        <div id="per_transaction" class="tab-pane fade active show">
+                            <h5>Per Transaction Report</h5>
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <form id='frm_generate'>
+                                        <div class="form-group row">
+                                            <div class="col">
+                                                <label><strong>Customer</strong></label>
+                                                <div>
+                                                    <select class="form-control form-control-sm select2" id="customer_id" name="input[customer_id]">
+                                                        <option class="-1">&mdash; All &mdash; </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <label><strong>Product</strong></label>
+                                                <div>
+                                                    <select class="form-control form-control-sm select2" id="product_id" name="input[product_id]">
+                                                        <option class="-1">&mdash; All &mdash; </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <label>&nbsp;</label>
+                                                <div>
+                                                    <div class="btn-group pull-right">
+                                                        <button type="submit" id="btn_generate" class="btn btn-primary btn-sm btn-icon-split">
+                                                            <span class="icon text-white-50">
+                                                                <i class="fas fa-plus"></i>
+                                                            </span>
+                                                            <span class="text">Generate</span>
+                                                        </button>
+                                                        <button type="button" onclick="exportTableToExcel(this,'dt_entries','Stock-Releasal-Report')" class="btn btn-success btn-sm btn-icon-split">
+                                                            <span class="icon">
+                                                                <i class="ti ti-cloud-down"></i>
+                                                            </span>
+                                                            <span class="text"> Export</span>
+                                                        </button>
+                                                        <button onclick="print_report('report_container')" type="button" class="btn btn-info btn-sm btn-icon-split">
+                                                            <span class="icon text-white-50">
+                                                                <i class="fas fa-trash"></i>
+                                                            </span>
+                                                            <span class="text">Print</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div id="report_container" class="card-body">
+                                    <center>
+                                        <h4 class="report-header"><span id="company_name_label"></span></h4>
+                                        <h6 class="report-header"><span id="company_address_label" style="word-wrap: break-word;"></span></h6>
+                                        <h5 class="report-header">Stock Releasal Report</h5><br>
+                                    </center>
+                                    <div class="table-responsive" id="dt_entries">
 
-            <div class="col-12 col-xl-12 card shadow mb-4">
-                <div class="card-header py-3">
-                    <form id='frm_generate'>
-                        <div class="form-group row">
-                            <div class="col">
-                                <label><strong>Customer</strong></label>
-                                <div>
-                                    <select class="form-control form-control-sm select2" id="customer_id" name="input[customer_id]">
-                                        <option class="-1">&mdash; All &mdash; </option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <label><strong>Product</strong></label>
-                                <div>
-                                    <select class="form-control form-control-sm select2" id="product_id" name="input[product_id]">
-                                        <option class="-1">&mdash; All &mdash; </option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <label>&nbsp;</label>
-                                <div>
-                                    <div class="btn-group pull-right">
-                                        <button type="submit" id="btn_generate" class="btn btn-primary btn-sm btn-icon-split">
-                                            <span class="icon text-white-50">
-                                                <i class="fas fa-plus"></i>
-                                            </span>
-                                            <span class="text">Generate</span>
-                                        </button>
-                                        <button type="button" onclick="exportTableToExcel(this,'dt_entries','Stock-Releasal-Report')" class="btn btn-success btn-sm btn-icon-split">
-                                            <span class="icon">
-                                                <i class="ti ti-cloud-down"></i>
-                                            </span>
-                                            <span class="text"> Export</span>
-                                        </button>
-                                        <button onclick="print_report('report_container')" type="button" class="btn btn-info btn-sm btn-icon-split">
-                                            <span class="icon text-white-50">
-                                                <i class="fas fa-trash"></i>
-                                            </span>
-                                            <span class="text">Print</span>
-                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </form>
-                </div>
-                <div id="report_container" class="card-body">
-                    <center>
-                        <h4 class="report-header"><span id="company_name_label"></span></h4>
-                        <h6 class="report-header"><span id="company_address_label" style="word-wrap: break-word;"></span></h6>
-                        <h5 class="report-header">Stock Releasal Report</h5><br>
-                    </center>
-                    <div class="table-responsive" id="dt_entries">
-                        
+
+                        <div id="per_item" class="tab-pane fade">
+                            <h5>Per Item Stock Releasal Report</h5>
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <form id='frm_generate_item'>
+                                        <div class="form-group row">
+                                            <div class="col">
+                                                <label><strong>Product</strong></label>
+                                                <div>
+                                                    <select class="form-control form-control-sm select2" id="product_id_2" name="input[product_id_2]">
+                                                        <option class="-1">&mdash; All &mdash; </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <label>&nbsp;</label>
+                                                <div>
+                                                    <div class="btn-group pull-right">
+                                                        <button type="submit" id="btn_generate" class="btn btn-primary btn-sm btn-icon-split">
+                                                            <span class="icon text-white-50">
+                                                                <i class="fas fa-plus"></i>
+                                                            </span>
+                                                            <span class="text">Generate</span>
+                                                        </button>
+                                                        <button type="button" onclick="exportTableToExcel(this,'dt_entries','Stock-Releasal-Report')" class="btn btn-success btn-sm btn-icon-split">
+                                                            <span class="icon">
+                                                                <i class="ti ti-cloud-down"></i>
+                                                            </span>
+                                                            <span class="text"> Export</span>
+                                                        </button>
+                                                        <button onclick="print_report('report_container')" type="button" class="btn btn-info btn-sm btn-icon-split">
+                                                            <span class="icon text-white-50">
+                                                                <i class="fas fa-trash"></i>
+                                                            </span>
+                                                            <span class="text">Print</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div id="report_container" class="card-body">
+                                    <center>
+                                        <h4 class="report-header"><span id="company_name_label"></span></h4>
+                                        <h6 class="report-header"><span id="company_address_label" style="word-wrap: break-word;"></span></h6>
+                                        <h5 class="report-header">Stock Releasal Report</h5><br>
+                                    </center>
+                                    <div class="table-responsive" id="dt_entries_item">
+                                    <thead>
+                                        <tr>
+                                            <th>ITEM</th>
+                                            <th style="text-align:right">QTY</th>
+                                            <th style="text-align:right">AMOUNT</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th colspan="1" style="text-align:right">Total:</th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                    </tfoot>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
             </div>
+
+
         </div>
     </div>
 </div>
@@ -75,6 +164,12 @@
         e.preventDefault();
 
         getReport();
+    });
+
+    $("#frm_generate_item").submit(function(e) {
+        e.preventDefault();
+
+        getReportItem();
     });
 
     function print_report(container, title) {
@@ -131,10 +226,30 @@
         });
     }
 
+    function getReportItem() {
+        var product_id = $("#product_id").val();
+
+        $("#dt_entries_item").html("<center><h3><span class='fa fa-spinner fa-spin'></span> Loading ...</h3></center>");
+
+        $.ajax({
+            type: "POST",
+            url: "controllers/sql.php?c=" + route_settings.class_name + "&q=per_item",
+            data: {
+                input: {
+                    product_id: product_id
+                }
+            },
+            success: function(data) {
+                $("#dt_entries_item").html(data);
+            }
+        });
+    }
+
     $(document).ready(function() {
         $("#company_name_label").html(company_profile.company_name);
         $("#company_address_label").html(company_profile.company_address);
         getSelectOption('Products', 'product_id', 'product_name', '', [], -1, 'All');
+        getSelectOption('Products', 'product_id_2', 'product_name', '', [], -1, 'All', 1);
         getSelectOption('Customers', 'customer_id', 'customer_name', '', [], -1, 'All');
     });
 </script>
