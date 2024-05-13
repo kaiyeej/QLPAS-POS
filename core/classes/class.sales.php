2d19ce0bce60d629db8b21fc7c8328537d6e87df
Jeffred Lim <58154782+jepjepj@users.noreply.github.com>
2024-05-13 21:16:04 +0800

    test response

--- core/classes/class.sales.php
+++ core/classes/class.sales.php
@@ -355,19 +355,19 @@ class Sales extends Connection
     }
 
     public function script_total_sales_runner(){
-        $result = "";
-        $fetch = $this->select($this->table, "sales_id", "sales_id > 0 AND sales_id <= 1000");
-        while($row = $fetch->fetch_assoc()){
-            $total_sales_amount = total_sales($row['sales_id']);
-            $res = $this->update($this->table, ['total_sales_amount' => $total_sales_amount], "sales_id='$row[sales_id]'");
-            if($res){
-                $response = "Updated sales id: " . $row['sales_id'] . " amount = " . $total_sales_amount;
-            }else{
-                $response = "(X) ----> Updated sales id: " . $row['sales_id'];
-            }
+        $result = "test";
+        // $fetch = $this->select($this->table, "sales_id", "sales_id > 0 AND sales_id <= 1000");
+        // while($row = $fetch->fetch_assoc()){
+        //     $total_sales_amount = total_sales($row['sales_id']);
+        //     $res = $this->update($this->table, ['total_sales_amount' => $total_sales_amount], "sales_id='$row[sales_id]'");
+        //     if($res){
+        //         $response = "Updated sales id: " . $row['sales_id'] . " amount = " . $total_sales_amount;
+        //     }else{
+        //         $response = "(X) ----> Updated sales id: " . $row['sales_id'];
+        //     }
 
-            $result .= $response . "\n";
-        }
+        //     $result .= $response . "\n";
+        // }
 
         return $result;
     }
