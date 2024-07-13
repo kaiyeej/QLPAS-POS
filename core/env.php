1ba45490153362579bb1add1826a8acab2c00d16
John Mark <58464475+jm-johnmark@users.noreply.github.com>
2024-07-13 14:41:02 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -1,17 +1,17 @@
 <?php
 
-define('APP_FOLDER', 'QLPAS-POS');
+define('APP_FOLDER', 'posy');
 define('BASE_URL', 'http://localhost/' . APP_FOLDER . '/');
 define("BASE_PATH", __DIR__ . "/../");
 define('DEVELOPMENT', true);
 
-define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "qlpas3");
-
-
 // define("HOST", "localhost");
-// define("USER", "u814036432_jcis_root");
-// define("PASSWORD", "@Jcis123!");
-// define("DBNAME", "u814036432_jcis_db");
+// define("USER", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "qlpas3");
+
+
+define("HOST", "localhost");
+define("USER", "u814036432_jcis_root");
+define("PASSWORD", "@Jcis123!");
+define("DBNAME", "u814036432_jcis_db");
