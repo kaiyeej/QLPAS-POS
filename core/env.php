4b58fea80a08c09dde306cc302a646452c24d6dd
John Mark <58464475+jm-johnmark@users.noreply.github.com>
2024-07-13 14:42:18 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_FOLDER', 'posy');
+define('APP_FOLDER', 'pos');
 define('BASE_URL', 'http://localhost/' . APP_FOLDER . '/');
 define("BASE_PATH", __DIR__ . "/../");
 define('DEVELOPMENT', true);
@@ -8,7 +8,7 @@ define('DEVELOPMENT', true);
 // define("HOST", "localhost");
 // define("USER", "root");
 // define("PASSWORD", "");
-// define("DBNAME", "qlpas3");
+// define("DBNAME", "jcis_db");
 
 
 define("HOST", "localhost");
