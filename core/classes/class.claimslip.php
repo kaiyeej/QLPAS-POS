a7701b10638ef7234fb56ad542b5f80a14e7b9db
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2025-01-25 19:03:23 +0800

    claim slip status

--- core/classes/class.claimslip.php
+++ core/classes/class.claimslip.php
@@ -96,7 +96,8 @@ class ClaimSlip extends Connection
                 $this->inputs['total_amount'] = $Sales->total($sales_id);
                 $this->inputs[$this->name] = $this->generate();
                 $this->inputs['withdrawal_id'] = $withdrawal_id;
-                $this->inputs['status'] = 'P';
+                // $this->inputs['status'] = 'P';
+                $this->inputs['status'] = 'F';
                 $this->add();
 
                 // update sales withdrawal status
@@ -205,7 +206,8 @@ class ClaimSlip extends Connection
     {
         $form = array(
             'withdrawal_id'   => $this->inputs['withdrawal_id'],
-            'status' => 'P',
+            // 'status' => 'P',
+            'status' => 'F',
             'checked_by' => $this->inputs['checked_by']
         );
         $primary_id = $this->inputs[$this->pk];
